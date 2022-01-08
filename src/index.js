4100b7806d1c3616e8967c57dee8d1c4c8943c0c
Marek Hauschwitz <mhau@outlook.cz>
2022-01-08 21:48:38 +0100

    pridano loggovani HTTP, ChalkJS

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import express from 'express'
 import { connect as connectDB } from './db'
 import indexRouter from './routes/indexRouter'
 import postInfoRouter from './routes/passwordPostRouter'
+import morgan from 'morgan'
+import chalk from 'chalk'
 
 const app = express(),
     path = process.cwd(),
     port = 3021
 
+const morganChalk = morgan(function (tokens, req, res) {
+    return [
+        chalk.green.bold(tokens.method(req, res)),
+        chalk.red.bold(tokens.status(req, res)),
+        chalk.white(tokens.url(req, res)),
+        chalk.yellow(tokens['response-time'](req, res) + ' ms'),
+    ].join(' ')
+})
+
+app.use(morganChalk)
 app.use('/', indexRouter)
 app.use('/register', postInfoRouter)
 
